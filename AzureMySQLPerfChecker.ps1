1f80298cfc83660b2874c9f7eff82035dd3241a4
Shawn Xiao <xixia@microsoft.com>
2023-10-25 17:50:39 +0800

    update ps namespace

--- AzureMySQLPerfChecker.ps1
+++ AzureMySQLPerfChecker.ps1
@@ -1,4 +1,17 @@
-﻿
+﻿#Azure MySQL Perf Checker
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using namespace System
+using namespace System.Net
+using namespace System.net.Sockets
+using namespace System.Collections.Generic
+using namespace System.Diagnostics
+using namespace Microsoft.Azure.PowerShell.Cmdlets.MySql
+using namespace MySql.Data.MySqlClient
+
 $RepositoryBranch = 'master'
 
 $CustomerRunningInElevatedMode = $false
